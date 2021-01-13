c4570fed569906ac5b72c75a7fec94146ba7455f
xxAgent <Jack.sTreasure@protonmail.com>
2021-01-13 22:05:47 +0800

    Week13 homework

---
+++ Week_13/activemq-demo/src/main/java/com/example/jms/JmsConsumer.java
@@ -0,0 +1,14 @@
+package com.example.jms;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class JmsConsumer {
+    @JmsListener(destination = "activeTest")
+    public void receiveMessage(final Map message) {
+        System.out.println(message.toString());
+    }
+}
